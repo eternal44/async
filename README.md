9dc8d1fe754fb576460b2691ff1fa37cd52b81fe
James Youn <jamesyoun710@gmail.com>
2016-05-27 12:36:35 -0700

    Add times.js

---
+++ README.md
@@ -0,0 +1,2 @@
+# nodeschool.io's async module.  
+JavaScript handles errors differently when using promises & callbacks [(click for reference)](https://www.joyent.com/developers/node/design/errors).  This is a portion of my exploration into using the 'async' module as an alternative to promises and to avoid 'callback hell'.
--- times.js
+++ times.js
@@ -1,67 +1,66 @@
-var http = require('http')
 var async = require('async')
-var hostName = process.argv[2]
+var http = require('http')
+var hostname = process.argv[2]
 var port = process.argv[3]
-var url = 'http://' + hostName + ':' + port
+var url = 'http://' + hostname + ':' + port
 
 function createUser(id, done) {
-  var opts = {
-    hostname: hostName,
+  var newUser = {user_id: id + 1}
+
+  var options = {
+    hostname: hostname,
     port: port,
-    path: '/users/create',
-    method: 'POST'
+    path: url + '/users/create',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
   }
 
-  var newUser = {
-    user_id: id + 1
-  }
-  newUser = JSON.stringify(newUser)
-
-  var body = '';
-  var req = http.request(opts, function(res) {
-    res.on('data', function(chunk) {
+  var req = http.request(options, function(res) {
+    var body = ''
+    res.setEncoding('utf8')
+    res.on('data', function(chunk){
       body += chunk.toString()
     })
-    res.on('end', function() {
-      done()
+    res.on('end', function(){
+      done(null, body)
     })
   })
 
-  req.on('error', function(err){
-    done(err)
+  req.on('error', function(err) {
+    console.error(`Problem with request: ${err.message}`)
+    done()
   })
 
-  req.write(newUser)
+  req.write(JSON.stringify(newUser))
   req.end()
 }
 
-async.series({  
+async.series({
   post: function(done) {
-    async.times(5, function(count, done){
+    async.times(5, function(count, done) {
       createUser(count, function(err, user) {
+        if(err) return console.log('POST error:', err)
         done(err, user)
       })
-    }, function(err, users) {
-      if(err) return done(err)
-      done(null, 'saved')
-      // we should have 5 users here
+    }, function(err, users){
+      done(null, users)
     })
   },
+
   get: function(done) {
+    var body = ''
     http.get(url + '/users', function(res) {
-      var body = ''
-      res.on('data', function(chunk){
+      res.on('data', function(chunk) {
         body += chunk.toString()
       })
       res.on('end', function() {
         done(null, body)
       })
-    }).on('error', done)
+    }).on('err', done)
   }
 }, function(err, results) {
-  if(err) return console.log(err)
-  console.log(results.get)
+  if(err) return console.log('GET error:', err)
+    console.log(results.get)
 })
-
-
-
